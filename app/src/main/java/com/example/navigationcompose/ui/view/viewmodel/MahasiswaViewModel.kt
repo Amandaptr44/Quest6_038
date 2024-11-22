7bd2aea16ce7ff5ea306d12a3e34b4a0723db0f5
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-11-22 17:05:46 +0700

    Tambahkan kolom dengan warna latar belakang dan rata tengah

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,3 +1,27 @@
 package com.example.navigationcompose.ui.view.viewmodel
 
-data class MahasiswaViewModel()
+import androidx.lifecycle.ViewModel
+import com.example.navigationcompose.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel: ViewModel() {
+
+    //requ
+    private val mahasiswaStateUi =
+        MutableStateFlow(Mahasiswa())
+
+    val MahasiswaUiState: StateFlow<Mahasiswa> = mahasiswaStateUi.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>){
+        mahasiswaStateUi.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2]
+            )
+        }
+    }
+}
